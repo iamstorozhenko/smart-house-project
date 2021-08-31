7862574043a6f48313fbed3bd0b5b7340203b0c9
anton <nba15melo@gmail.com>
2021-09-01 00:37:08 +0300

    added basic btns and style

---
+++ script.js
@@ -0,0 +1,147 @@
+
+// class MainComponent {
+//   constructor() {}
+//   renderBackround() {
+//     const backgr = document.createElement("div");
+//     backgr.id = "backgr";
+//   }
+//   renderText() {
+//     const text = document.createElement("p");
+//     text.id = "text";
+//     text.innerHTML = "Light";
+//   }
+//   renderIcon() {
+//     const icon = document.createElement("p");
+//     icon.id = "icon";
+//     icon.innerHTML = '<i id="bulb" class="far fa-lightbulb"></i>';
+//   }
+
+// }
+
+// const lightBtn = new MainComponent();
+// lightBtn.renderBackround()
+
+class MainOption {
+  constructor() {}
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "Light";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i id="bulb" class="far fa-lightbulb"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.prepend(backgr);
+  }
+}
+
+const mainOpt = new MainOption();
+mainOpt.render();
+
+class Curtains extends MainOption {
+  constructor() {
+    super();
+  }
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "Curtains";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i class="fas fa-adjust"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.append(backgr);
+  }
+}
+const curtainsOpt = new Curtains();
+curtainsOpt.render();
+
+class Garage extends MainOption {
+  constructor() {
+    super();
+  }
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "Curtains";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i class="fas fa-warehouse"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.append(backgr);
+  }
+}
+const garage = new Garage();
+garage.render();
+
+class Temperature extends MainOption {
+  constructor() {
+    super();
+  }
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "Temperature";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i class="fas fa-temperature-high"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.append(backgr);
+  }
+}
+const temperature = new Temperature();
+temperature.render();
+
+class Tv extends MainOption {
+  constructor() {
+    super();
+  }
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "TV";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i class="fas fa-tv"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.append(backgr);
+  }
+}
+const tv = new Tv();
+tv.render();
+
+class Music extends MainOption {
+  constructor() {
+    super();
+  }
+  render() {
+    const backgr = document.createElement("div");
+    backgr.id = "btns";
+    const text = document.createElement("p");
+    text.id = "text";
+    text.innerHTML = "Music";
+    backgr.append(text);
+    const icon = document.createElement("p");
+    icon.innerHTML = '<i class="fas fa-music"></i>';
+    backgr.append(icon);
+    const getContainer = document.getElementById("container");
+    getContainer.append(backgr);
+  }
+}
+const music = new Music();
+music.render();
