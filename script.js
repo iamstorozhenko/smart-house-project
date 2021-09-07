ae11583dbe4f0c4abffebf3dfea0cf790c89919a
anton <nba15melo@gmail.com>
2021-09-07 20:03:29 +0300

    added functionality for curtains button

--- script.js
+++ script.js
@@ -19,21 +19,21 @@ const mainOpt = new MainOption();
 mainOpt.render();
 
 const getLight = document.getElementById("btn-light");
-getLight.addEventListener("click", addLightSettings);
+getLight.addEventListener("click", lightSettings);
 
-function addLightSettings(e) {
+function lightSettings(e) {
   console.log(e.target);
-  const lightSettings = document.createElement("div");
-  lightSettings.id = "choose-light-color";
+  const curtainsSettings = document.createElement("div");
+  curtainsSettings.id = "choose-light-color";
   const text = document.createElement("p");
   text.innerHTML = "Lighting";
   const crossExit = document.createElement("div");
   crossExit.classList.add("cross");
   crossExit.innerHTML = '<i class="fas fa-times"></i>';
   text.append(crossExit);
-  lightSettings.prepend(text);
+  curtainsSettings.prepend(text);
   const getBackground = document.getElementById("space");
-  getBackground.prepend(lightSettings);
+  getBackground.prepend(curtainsSettings);
 
   const deleteBtn = document.getElementsByClassName("cross")[0];
   deleteBtn.addEventListener("click", deleteCross);
@@ -57,16 +57,16 @@ function addLightSettings(e) {
   const inputOnOff = document.createElement("input");
   inputOnOff.type = "checkbox";
   inputOnOff.id = "input-checker";
-  lightSettings.append(inputOnOff);
+  curtainsSettings.append(inputOnOff);
 
   const getText = document.createElement("p");
   getText.id = "getRandomColorText";
   getText.innerHTML = "Get random light color: ";
-  lightSettings.append(getText);
+  curtainsSettings.append(getText);
 
   const btnWrapper = document.createElement("div");
   btnWrapper.id = "btn-wrapper";
-  lightSettings.append(btnWrapper);
+  curtainsSettings.append(btnWrapper);
 
   const randomBtn = document.createElement("button");
   randomBtn.id = "btn-random";
@@ -125,6 +125,93 @@ class Curtains extends MainOption {
 }
 const curtainsOpt = new Curtains();
 curtainsOpt.render();
+
+const getCurtains = document.getElementById("btn-curtains");
+getCurtains.addEventListener("click", curtainsSettings);
+
+function curtainsSettings(e) {
+  console.log(e.target);
+  const curtainsSettings = document.createElement("div");
+  curtainsSettings.id = "choose-light-color";
+  const text = document.createElement("p");
+  text.innerHTML = "Curtains";
+  const crossExit = document.createElement("div");
+  crossExit.classList.add("cross");
+  crossExit.innerHTML = '<i class="fas fa-times"></i>';
+  text.append(crossExit);
+  curtainsSettings.prepend(text);
+  const getBackground = document.getElementById("space");
+  getBackground.prepend(curtainsSettings);
+
+  const deleteBtn = document.getElementsByClassName("cross")[0];
+  deleteBtn.addEventListener("click", deleteCross);
+
+  function deleteCross(e) {
+    const item = e.target;
+    if (item.classList[0] === "cross") {
+      const crossDelete = item.parentElement.parentElement;
+      crossDelete.remove();
+      const btn = document.getElementById("btn-curtains");
+      btn.disabled = false;
+    }
+  }
+
+  function clicked() {
+    const btn = document.getElementById("btn-curtains");
+    btn.disabled = true;
+  }
+  clicked();
+
+  const inputOnOffCurtains = document.createElement("input");
+  inputOnOffCurtains.type = "checkbox";
+  inputOnOffCurtains.id = "input-checker-curtains";
+  curtainsSettings.append(inputOnOffCurtains);
+
+  //   const getText = document.createElement("p");
+  //   getText.id = "getRandomColorText";
+  //   getText.innerHTML = "Get random light color: ";
+  //   curtainsSettings.append(getText);
+
+  //   const btnWrapper = document.createElement("div");
+  //   btnWrapper.id = "btn-wrapper";
+  //   curtainsSettings.append(btnWrapper);
+
+  //   const randomBtn = document.createElement("button");
+  //   randomBtn.id = "btn-random";
+  //   randomBtn.innerHTML = '<i class="fas fa-random"></i>';
+  //   btnWrapper.append(randomBtn);
+
+  //   const createRandomColor = document.createElement("div");
+  //   createRandomColor.id = "random-color";
+  //   btnWrapper.append(createRandomColor);
+
+  //   const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  //   randomBtn.addEventListener("click", () => {
+  //     let hex = "#";
+  //     for (let i = 0; i < 6; i++) {
+  //       hex += hexColor[getRandomColor()];
+  //     }
+  //     createRandomColor.style.backgroundColor = hex;
+  //   });
+  //   function getRandomColor() {
+  //     return Math.floor(Math.random() * hexColor.length);
+  //   }
+
+  //   inputOnOffCurtains.addEventListener("click", checkOnOff);
+
+  //   function checkOnOff() {
+  //     if (inputOnOffCurtains.checked == true) {
+  //       document.getElementById("btn-random").disabled = false;
+  //       document.getElementById("btn-random").style.opacity = 1;
+  //       document.getElementById("random-color").style.opacity = 1;
+  //     } else if (inputOnOffCurtains.checked == false) {
+  //       document.getElementById("btn-random").disabled = true;
+  //       document.getElementById("btn-random").style.opacity = 0.3;
+  //       document.getElementById("random-color").style.opacity = 0.3;
+  //     }
+  //   }
+  //   checkOnOff();
+}
 
 class Garage extends MainOption {
   constructor() {
